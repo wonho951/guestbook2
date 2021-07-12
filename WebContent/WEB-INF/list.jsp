e5b5d27b179bbff22b863276513d4585c6bd2343
wonho <wonho951@gmail.com>
2021-07-12 21:28:08 +0900

    2021.07.12 model2(guestbook2 과제)

---
+++ WebContent/WEB-INF/list.jsp
@@ -0,0 +1,57 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+    
+<%@ page import="com.javaex.vo.GuestbookVo" %>
+<%@ page import="java.util.List" %>    
+
+
+<% 	List<GuestbookVo> guestbookList = (List<GuestbookVo>)request.getAttribute("gList");%>
+    
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	
+		<form action = "/guestbook2/gbc" method = "get">
+		<table border = "1">
+			<tr>
+				<td>이름</td>
+				<td><input type = "text" name = "name"></td>
+				<td>비밀번호</td>
+				<td><input type = "password" name = "password"></td>
+			</tr>
+		
+			<tr>
+				<td colspan = "5"><textarea name = "content"></textarea></td>
+			</tr>
+			
+			<tr>
+				<td colspan = "5"><button type = "submit">확인</button></td>
+			</tr>
+		</table>
+		<input type = "hidden" name = "action" value = "add">
+		</form>
+		<br>
+	
+	<% for(int i = 0; i < guestbookList.size(); i++) {%>		
+	<table border = "1">
+		<tr>
+			<td><%= guestbookList.get(i).getNo() %></td>
+			<td><%= guestbookList.get(i).getName() %></td>
+			<td><%= guestbookList.get(i).getRegDate() %></td>	
+			<td><a href="/guestbook2/gbc?action=deleteForm&no<%= guestbookList.get(i).getNo()%>">삭제</a></td>	<!-- 비밀번호 유출시킬 문제가 아닌이상 no만 해준다. -->
+		</tr>
+	
+		<tr>
+			<td colspan = "4"><%= guestbookList.get(i).getContent() %></td>
+		</tr>
+	</table>
+	<br>
+	
+	<%} %>
+
+</body>
+</html>
--- src/com/javaex/controller/GuestbookController.java
+++ src/com/javaex/controller/GuestbookController.java
@@ -1,26 +1,60 @@
 package com.javaex.controller;
 
 import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.javaex.dao.GuestbookDao;
+import com.javaex.vo.GuestbookVo;
+
 
 @WebServlet("/gbc")
 public class GuestbookController extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-
-    public GuestbookController() {
-        super();
-        
-    }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("컨트롤러");
 		
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		//파라미터의 action 값을 읽어온다.
+		String action = request.getParameter("action");
+		System.out.println(action);
+		
+		if ("list".equals(action)) {
+			System.out.println("[리스트]");
+			
+			GuestbookDao guestbookDao = new GuestbookDao(); 
+			List<GuestbookVo> guestbookList = guestbookDao.getguestList();
+			
+			System.out.println("controller-------------------------");
+			System.out.println(guestbookList);
+			
+			
+			request.setAttribute("gList", guestbookList);
+			
+			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/list.jsp");
+			rd.forward(request, response);	
+			
+		} else if ("add".equals(action)) {
+			System.out.println("등록");
+			
+			GuestbookDao guestDao = new GuestbookDao();
+			
+			String name = request.getParameter("name");
+			String password = request.getParameter("password");
+			String content = request.getParameter("content");
+			
+			GuestbookVo guestVo = new GuestbookVo(name, password, content);
+			
+			guestDao.guestInsert(guestVo);
+			
+			response.sendRedirect("/guestbook2/gbc?action=list");	
+			
+		}
 	}
 
 
